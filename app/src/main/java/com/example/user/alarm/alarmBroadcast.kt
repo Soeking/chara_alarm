b150b137efe1befc93258e9c958afb5e155ee908
Tim Olson <mr.kaito01@gmail.com>
2019-01-22 00:36:55 +0900

    first

---
+++ app/src/main/java/com/example/user/alarm/alarmBroadcast.kt
@@ -0,0 +1,15 @@
+package com.example.user.alarm
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class AlarmBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val intent = Intent(context, MainActivity::class.java)
+            .putExtra("onReceive", true)
+            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        context?.startActivity(intent)
+    }
+}
